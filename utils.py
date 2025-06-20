835bfce05928a9123e6a5b5727e0a4afcca63c4c
Karanja Kariuki <kkariuki2007@gmail.com>
2025-06-20 10:57:11 +0300

    Created Write to Json File function, generate ID function and get todays tasks function

--- utils.py
+++ utils.py
@@ -11,3 +11,18 @@ def load_tasks():
     with open(TASK_FILE, "r") as f:
         data = json.load(f)
     return [Task(**task) for task in data]
+
+def save_tasks(tasks):
+    with open(TASK_FILE, "w") as f:
+        json.dump([task.to_dict() for task in tasks], f, indent=2)
+
+def generate_id(tasks):
+    for task in tasks:
+        if task.id == max(task.id for task in tasks):
+            return task.id + 1
+        elif not task.id:
+            return 1
+
+def get_today_tasks(tasks):
+    today = date.today().isoformat()
+    return [task for task in tasks if task.due_date == today]
